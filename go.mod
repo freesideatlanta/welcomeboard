a27445309ecf0faa4985d4df8c3bf9209fb01e9f
Nathaniel Maile <npmaile@gmail.com>
2024-07-01 16:33:48 -0400

    change config from env vars to config.toml

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.22.4
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 )
--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"embed"
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
 	"os"
 
 	"github.com/freesideatlanta/welcomeboard/content"
+	toml "github.com/pelletier/go-toml/v2"
 )
 
 //go:embed static
@@ -24,32 +26,53 @@ var pemString string
 var memberID string
 var consumerKey string
 
+type Config struct {
+	MeetupKey    string
+	MeetupSecret string
+	PemString    string
+	MemberID     string
+	ConsumerKey  string
+}
+
 func init() {
-	var err error
+	var cfg Config
+	cfgFile, err := os.OpenFile("./config.toml", os.O_RDONLY, 0777)
+	if err != nil {
+		panic("unable to read config.toml " + err.Error())
+	}
+	cfgFileContents, err := io.ReadAll(cfgFile)
+	if err != nil {
+		panic("unable to read config.toml file contents " + err.Error())
+	}
+	err = toml.Unmarshal(cfgFileContents, &cfg)
+	if err != nil {
+		panic("unable to decode config.toml file contents " + err.Error())
+	}
+
 	templates, err = template.ParseFS(templatesFS, "templates/*")
 	if err != nil {
 		panic(err)
 	}
 
-	meetupKey = os.Getenv("MEETUP_KEY")
+	meetupKey = cfg.MeetupKey
 	if meetupKey == "" {
-		panic("no meetup key provided (env var MEETUP_KEY)")
+		panic("no meetup key provided (MeetupKey in toml)")
 	}
-	meetupSecret = os.Getenv("MEETUP_SECRET")
+	meetupSecret = cfg.MeetupSecret
 	if meetupKey == "" {
-		panic("no meetup secret provided (env var MEETUP_SECRET)")
+		panic("no meetup secret provided (MeetupSecret in toml")
 	}
-	pemString = os.Getenv("MEETUP_PEM_STRING")
+	pemString = cfg.PemString
 	if pemString == "" {
-		panic("no meetup pem string provided (env var MEETUP_PEM_STRING)")
+		panic("no meetup pem string provided (PemString in toml)")
 	}
-	memberID = os.Getenv("MEETUP_MEMBER_ID")
+	memberID = cfg.MemberID
 	if memberID == "" {
-		panic("no meetup memberID string provided (env var MEETUP_MEMBER_ID)")
+		panic("no meetup memberID string provided (MemberID in toml)")
 	}
-	consumerKey = os.Getenv("MEETUP_CONSUMER_KEY")
+	consumerKey = cfg.ConsumerKey
 	if consumerKey == "" {
-		panic("no meetup pem string provided (env var MEETUP_CONSUMER_KEY)")
+		panic("no meetup pem string provided (ConsumerKey in toml)")
 	}
 
 }
